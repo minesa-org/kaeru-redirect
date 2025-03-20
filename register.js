17f7381563526ac11b68e03e9c46b342829268da
İbrahim <neodevils_contact@icloud.com>
2025-03-20 22:21:39 +0300

    Update register.js

--- register.js
+++ register.js
@@ -6,33 +6,9 @@ const url = `https://discord.com/api/v10/applications/${process.env.CLIENT_ID}/r
 
 const body = [
   {
-    key: "is_dev",
-    name: "engineer",
-    description: "A role for developers. Claimable by game devs, website devs, and security managers.",
-    type: 7, // boolean_eq
-  },
-  {
-    key: "is_mod",
-    name: "enforcer",
-    description: "A role for moderators. Claimable by users who moderate content.",
-    type: 7, // boolean_eq
-  },
-  {
-    key: "is_ads",
-    name: "strategist",
-    description: "A role for advertisers. Claimable by users who manage ads or promotions.",
-    type: 7, // boolean_eq
-  },
-  {
-    key: "is_admin",
-    name: "director",
-    description: "A role for administrators. Claimable by users who have admin privileges.",
-    type: 7, // boolean_eq
-  },
-  {
-    key: "is_owner",
-    name: "founder",
-    description: "A role for owners. Claimable by users who are the owner of the project.",
+    key: "is_staff",
+    name: "Staff",
+    description: "A role for team members. Claimable by Minesa Team.",
     type: 7, // boolean_eq
   },
 ];
